3e7ac38ca1c1f03cd00cee0587d203b18c3143b1
Spencer Fornaciari <mail@spencerfornaciari.com>
2013-10-26 17:08:53 -0700

    Initial Commit

---
+++ MySocialApp/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  MySocialApp
+//
+//  Created by Spencer Fornaciari on 10/26/13.
+//  Copyright (c) 2013 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
---
+++ MySocialApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySocialApp
+//
+//  Created by Spencer Fornaciari on 10/26/13.
+//  Copyright (c) 2013 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFAppDelegate class]));
+    }
+}
